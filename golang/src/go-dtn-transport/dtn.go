21494bdeb634797e421c17c5bdeb637b79f8e832
morriswinkler <m.winkler@open3dengineering.org>
2018-10-21 15:00:33 +0200

    add dtn transport layout

--- golang/src/go-dtn-transport/dtn.go
+++ golang/src/go-dtn-transport/dtn.go
@@ -1,36 +1,135 @@
-package dtn
+package main
 
 import (
+	"time"
 	"context"
-	"log"
 	"net"
 
 	peer "github.com/libp2p/go-libp2p-peer"
-	tpt "github.com/libp2p/go-libp2p-transport"
-	tptu "github.com/libp2p/go-libp2p-transport-upgrader"
 	ma "github.com/multiformats/go-multiaddr"
 )
 
-type DtnTransport struct {
-	Upgrader *tptu.Upgrader
+
+type DtnStream struct {
+
 }
 
-func New(u *tptu.Upgrader) *DtnTransport {
-	return &DtnTransport{u}
+func (ds *DtnStream) Write(p []byte) (n int, err error) {
+
+	return 0, nil
+	}
+
+func (ds *DtnStream) Read(p []byte) (n int, err error) {
+	return 0, nil
+
+	}
+
+
+func (ds *DtnStream) 	Close() error {
+	return nil
 }
 
-func (c *DtnConn) Dial(network, address string) (Conn, Error) {
+
+// Reset closes both ends of the stream. Use this to tell the remote
+// side to hang up and go away.
+
+func (ds *DtnStream)  Reset() error {
+	return nil
 }
 
-func (t *DtnTransport) Dial(ctx context.Context, addr ma.Multiaddr, p peer.ID) (tpt.Conn, error) {
+
+func (ds *DtnStream) SetDeadline(time.Time) error {
+	return nil
 }
 
-func (t *DtnTransport) CanDial(addr ma.Multiaddr) bool {
-	// TODO implement
-	return true
+func (ds *DtnStream) SetReadDeadline(time.Time) error {
+	return nil
 }
 
-func (t *DtnTransport) Listen(addr ma.Multiaddr) (tpt.Listener, error) {
+func (ds *DtnStream) SetWriteDeadline(time.Time) error{
+	return nil
 }
 
-type dialer struct{}
+
+
+
+type DtnConn struct {}
+func (dc *DtnConn) 	Close() error {
+	return nil
+}
+
+// IsClosed returns whether a connection is fully closed, so it can
+// be garbage collected.
+func (dc *DtnConn)  IsClosed() bool {
+	return false
+}
+
+// OpenStream creates a new stream.
+func (dc *DtnConn)  OpenStream() (DtnStream, error) {
+	return DtnStream{}, nil
+	}
+
+// AcceptStream accepts a stream opened by the other side.
+func (dc *DtnConn) AcceptStream()  (DtnStream, error) {
+	return DtnStream{}, nil
+}
+
+
+type DtnAddr struct {}
+
+func (da DtnAddr) Network() string {
+	return ""
+}
+func (da DtnAddr) String() string {
+	return ""
+}
+
+type DtnListener struct {}
+
+func (dn *DtnListener) Accept() (DtnConn, error) {
+	return DtnConn{}, nil
+}
+
+
+func (dn *DtnListener) Close() error {
+	return nil
+}
+
+
+func (dn *DtnListener) Addr() net.Addr {
+	return DtnAddr{}
+}
+
+
+type DtnTransport struct {}
+
+func NewDtnTransport() *DtnTransport {
+	return &DtnTransport{}
+}
+
+
+func (dt *DtnTransport) Dial(ctx context.Context, raddr ma.Multiaddr, p peer.ID) (DtnConn, error) {
+	return DtnConn{}, nil
+}
+
+// CanDial returns true if this transport knows how to dial the given
+// multiaddr.
+//
+// Returning true does not guarantee that dialing this multiaddr will
+// succeed. This function should *only* be used to preemptively filter
+// out addresses that we can't dial.
+func (dt *DtnTransport) CanDial(addr ma.Multiaddr) bool {
+	return false
+}
+
+// Listen listens on the passed multiaddr.
+func (dt *DtnTransport) Listen(laddr ma.Multiaddr) (DtnListener, error) {
+	return DtnListener{}, nil
+}
+
+// Protocol returns the set of protocols handled by this transport.
+//
+// See the Network interface for an explanation of how this is used.
+func (dt *DtnTransport) Protocols() []int {
+	return []int{}
+}
